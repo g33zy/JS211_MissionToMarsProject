aff2a2d005d3a660be185944f7aa6d82126c83fe
g33zy <stevejgaz@gmail.com>
2023-02-06 20:56:38 -0600

    final touch

--- main.js
+++ main.js
@@ -11,7 +11,35 @@ const jobTypes = {
 
 // Your code will go here
 
+class CrewMember {
+  constructor(name, job, specialSkill) {
+    this.name = name
+    this.job = job
+    this.specialSkill = specialSkill
+    this.ship = null
+  }
+  enterShip(ship){
+    this.ship = ship
+    this.ship.crew.push(this)
+  };
 
+}
+
+class Ship {
+  constructor (name, type, ability) {
+    this.name = name
+    this.type = type
+    this.ability = ability
+    this.crew = []
+  }
+  missionStatement(){
+    if(this.crew.length > 0) {
+      return this.ability
+    } else {
+      return "Can't perform a mission yet."
+    }
+  }
+}
 
 
 
@@ -68,3 +96,5 @@ if (typeof describe === 'function'){
     });
   });
 }
+
+
